1aa67cda4877c7c13e5891ebdd49f4c5c45192cc
obenson29 <107527299+obenson29@users.noreply.github.com>
2022-07-16 19:23:24 +0530

    Update Edit.jsx

--- src/Edit.jsx
+++ src/Edit.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { Link, useParams ,useHistory} from "react-router-dom";
 import useFetch from "./useFetch";
 
@@ -10,60 +10,55 @@ const Edit = () => {
 
     let {data : task , pending} = useFetch("http://localhost:4000/tasks/" + id);
 
-    // let uName = useRef("");
-    // let uTaskName = useRef("");
-    // let uTaskDetail = useRef("");
-    // let uStartDate = useRef("");
-    // let uEndDate = useRef("");
-
-
-    const [uName,setuName] = useState(task.userName)
-
-    
+    let uName = useRef("");
+    let uTaskName = useRef("");
+    let uTaskDetail = useRef("");
+    let uStartDate = useRef("");
+    let uEndDate = useRef("");
     
 
-    // const handleEdit = (e)=>{
-    //     e.preventDefault();
+    const handleEdit = (e)=>{
+        e.preventDefault();
 
-    //     let currentDate = new Date();
-    //     let startDate = new Date(uStartDate.current.value);
-    //     let endDate = new Date(uEndDate.current.value);
-    //     let status = "";
+        let currentDate = new Date();
+        let startDate = new Date(uStartDate.current.value);
+        let endDate = new Date(uEndDate.current.value);
+        let status = "";
 
-    //     if(currentDate < startDate)
-    //     {
-    //         status = "Pending";
-    //     }
-    //     else if(currentDate>=startDate && currentDate<=endDate)
-    //     {
-    //         status = "Ongoing"
-    //     }
-    //     else
-    //     {
-    //         status = "Completed";
-    //     }
+        if(currentDate < startDate)
+        {
+            status = "Pending";
+        }
+        else if(currentDate>=startDate && currentDate<=endDate)
+        {
+            status = "Ongoing"
+        }
+        else
+        {
+            status = "Completed";
+        }
 
-    //     const newTask = 
-    //     { 
-    //         userName : uName.current.value,
-    //         taskName : uTaskName.current.value,
-    //         taskDetail : uTaskDetail.current.value,
-    //         startDate : uStartDate.current.value,
-    //         endDate : uEndDate.current.value,
-    //         status
-    //     }
-    //     // console.log(newTask);
-    //     fetch("http://localhost:4000/tasks/"+id , 
-    //     {
-    //         method:"PUT",
-    //         headers:{"Content-Type":"application/json"},
-    //         body:JSON.stringify(newTask)
-    //     }
-    //     ).then(()=>{
-    //         alert("task edited successfully");
-    //         history.push("/tasklist");
-    // })
-    // }
+        const newTask = 
+        { 
+            userName : uName.current.value,
+            taskName : uTaskName.current.value,
+            taskDetail : uTaskDetail.current.value,
+            startDate : uStartDate.current.value,
+            endDate : uEndDate.current.value,
+            status
+        }
+        // console.log(newTask);
+        fetch("http://localhost:4000/tasks/"+id , 
+        {
+            method:"PUT",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(newTask)
+        }
+        ).then(()=>{
+            alert("task edited successfully");
+            history.push("/tasklist");
+    })
+    }
 
 
     return ( 
@@ -74,15 +69,15 @@ const Edit = () => {
         {task && <div>
             <form onSubmit={handleEdit}>
 
-            <label>User Name</label> <input type="text" defaultValue={task.userName} onChange={(e)=> {setuName(e.target.value)}} />
+            <label>User Name</label> <input type="text" ref={uName}/>
 
-            <label>Task Name</label> <input type="text"  />
+            <label>Task Name</label> <input type="text" ref={uTaskName} />
 
-            <label>Task Detail</label> <textarea  />
+            <label>Task Detail</label> <textarea ref={uTaskDetail} />
 
-            <label>Start Date</label> <input type="date" />
+            <label>Start Date</label> <input type="date" ref={uStartDate} />
 
-            <label>End Date</label> <input type="date"  />
+            <label>End Date</label> <input type="date" ref={uEndDate} />
 
             <input type="submit" value="Edit Task" />
 
